9c716d1a7edd3bdacdd8ab800c3606cf8af495d2
Håkon Harnes <hakon@harnes.dev>
2023-12-06 19:32:13 +0100

    ci: fix macos and windows tests

--- tests/util_spec.lua
+++ tests/util_spec.lua
@@ -2,7 +2,13 @@ local util = require("img-clip.util")
 
 describe("util", function()
   it("can get the directory path from the file path", function()
-    local dir_path = util.get_dir_path_from_filepath("/home/user/project/image.png")
-    assert.same("/home/user/project/", dir_path)
+    -- construct test input
+    local path_separator = package.config:sub(1, 1)
+    local test_path = "path" .. path_separator .. "to" .. path_separator .. "file.txt"
+    local expected_dir_path = "path" .. path_separator .. "to" .. path_separator
+
+    -- check if the correct directory path is returned
+    local dir_path = util.get_dir_path_from_filepath(test_path)
+    assert.equals(expected_dir_path, dir_path)
   end)
 end)
