71879d281d02fb95d89a5493909a193f70a0967e
Håkon Harnes <hakon@harnes.dev>
2023-12-08 23:27:26 +0100

    chore: add missing type annotations

--- lua/img-clip/util.lua
+++ lua/img-clip/util.lua
@@ -6,33 +6,36 @@ M.executable = function(command)
   return vim.fn.executable(command) == 1
 end
 
+---@param cmd string
+---@return string output
+---@return number exit_code
 M.execute = function(cmd)
-  M.debug("Executing: " .. cmd)
-
-  local output = vim.fn.system(cmd)
-  local exit_code = vim.v.shell_error
-
-  return output, exit_code
+  return vim.fn.system(cmd), vim.v.shell_error
 end
 
+---@param feature string
 M.has = function(feature)
   return vim.fn.has(feature) == 1
 end
 
+---@param msg string
 M.warn = function(msg)
   vim.notify(msg, vim.log.levels.WARN, { title = "img-clip" })
 end
 
+---@param msg string
 M.error = function(msg)
   vim.notify(msg, vim.log.levels.ERROR, { title = "img-clip" })
 end
 
+---@param msg string
 M.debug = function(msg)
   if config.options.debug then
     vim.notify(msg, vim.log.levels.DEBUG, { title = "img-clip" })
   end
 end
 
+---@param args string
 M.input = function(args)
   local completed, output = pcall(function()
     return vim.fn.input(args)
