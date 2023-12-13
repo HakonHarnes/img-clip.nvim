867e441835ac00c3132596c37602669b2e703871
Håkon Harnes <hakon@harnes.dev>
2023-12-14 00:11:10 +0100

    feat: allow nested config options and add drag_and_drop option

--- lua/img-clip/config.lua
+++ lua/img-clip/config.lua
@@ -1,30 +1,36 @@
 local M = {}
 
 local defaults = {
-  dir_path = "assets", -- directory path to save images to, can be relative (cwd or current file) or absolute
-  file_name = "%Y-%m-%d-%H-%M-%S", -- file name format (see lua.org/pil/22.1.html)
-  url_encode_path = false, -- encode spaces and special characters in file path
-  use_absolute_path = false, -- expands dir_path to an absolute path
-  relative_to_current_file = false, -- make dir_path relative to current file rather than the cwd
-  prompt_for_file_name = true, -- ask user for file name before saving, leave empty to use default
-  show_dir_path_in_prompt = false, -- show dir_path in prompt when prompting for file name
-  use_cursor_in_template = true, -- jump to cursor position in template after pasting
-  insert_mode_after_paste = true, -- enter insert mode after pasting the markup code
-  embed_image_as_base64 = false, -- paste image as base64 string instead of saving to file
-  max_base64_size = 10, -- max size of base64 string in KB
+  default = {
+    dir_path = "assets", -- directory path to save images to, can be relative (cwd or current file) or absolute
+    file_name = "%Y-%m-%d-%H-%M-%S", -- file name format (see lua.org/pil/22.1.html)
+    url_encode_path = false, -- encode spaces and special characters in file path
+    use_absolute_path = false, -- expands dir_path to an absolute path
+    relative_to_current_file = false, -- make dir_path relative to current file rather than the cwd
+    prompt_for_file_name = true, -- ask user for file name before saving, leave empty to use default
+    show_dir_path_in_prompt = false, -- show dir_path in prompt when prompting for file name
+    use_cursor_in_template = true, -- jump to cursor position in template after pasting
+    insert_mode_after_paste = true, -- enter insert mode after pasting the markup code
+    embed_image_as_base64 = false, -- paste image as base64 string instead of saving to file
+    max_base64_size = 10, -- max size of base64 string in KB
 
-  -- drag and drop options
-  drag_and_drop = true, -- enable drag and drop support
-  drag_and_drop_insert_mode = false, -- enable drag and drop support in insert mode
-  download_dropped_images = true, -- download image from url instead of just embedding the url
-  copy_dropped_images = false, -- copy dropped images to dir_path instead of just including the original path
+    template = "$FILE_PATH",
 
-  template = "$FILE_PATH",
+    drag_and_drop = {
+      enabled = false,
+      insert_mode = false,
+      copy_images = false,
+      download_images = true,
+    },
+  },
 
   markdown = {
     url_encode_path = true,
-    download_dropped_images = false, -- just include the link to the image
     template = "![$CURSOR]($FILE_PATH)",
+
+    drag_and_drop = {
+      download_images = false,
+    },
   },
 
   html = {
@@ -86,29 +92,42 @@ M.get_option = function(key, opts)
   local ft = vim.bo.filetype
   local val
 
+  local function extract_option(table, nested_key)
+    local keys = vim.split(nested_key, ".", { plain = true, trimempty = true })
+    for _, k in ipairs(keys) do
+      if table and table[k] ~= nil then
+        table = table[k] -- navigate into the nested structure
+      else
+        return nil -- option not found in this table
+      end
+    end
+    return table
+  end
+
   -- check options passed explicitly to pasteImage function
   if opts and opts[key] ~= nil then
     val = opts[key]
 
-  -- otherwise chck for filetype-specific option
-  elseif M.options[ft] and M.options[ft][key] ~= nil then
-    val = M.options[ft][key]
+  -- check for filetype-specific option, including nested ones
+  elseif M.options[ft] then
+    val = extract_option(M.options[ft], key)
+    if val == nil then
+      -- fallback to default if not found in filetype-specific options
+      val = extract_option(M.options["default"], key)
+    end
 
-  -- otherwise check for global option
-  elseif M.options[key] ~= nil then
-    val = M.options[key]
+  -- check for global option, including nested ones
+  elseif M.options["default"] then
+    val = extract_option(M.options["default"], key)
+  end
 
   -- return nil if no option found
-  else
+  if val == nil then
     vim.notify("No option found for " .. key .. ".", vim.log.levels.WARN, { title = "img-clip" })
     return nil
   end
 
-  if type(val) == "function" then
-    return val() -- execute the function and return its result
-  else
-    return val
-  end
+  return type(val) == "function" and val() or val -- execute if it's a function and return its result, otherwise return the value
 end
 
 function M.setup(opts)
