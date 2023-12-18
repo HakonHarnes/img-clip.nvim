2cebafacff60e2aac651c2e46e4e0be424b0c810
Håkon Harnes <hakon@harnes.dev>
2023-12-18 16:35:20 +0100

    chore: add debug print

--- plugin/img-clip.lua
+++ plugin/img-clip.lua
@@ -1,4 +1,5 @@
 local img_clip = require("img-clip")
+local config = require("img-clip.config")
 local drag_and_drop = require("img-clip.drag_and_drop")
 
 vim.api.nvim_create_user_command("PasteImage", function()
@@ -10,6 +11,10 @@ end, {})
 -- it will contain the path to the image or file, or a link to the image
 vim.paste = (function(overridden)
   return function(lines, phase)
+    if config.get_option("debug") then
+      print("Paste: " .. vim.inspect(lines))
+    end
+
     if #lines > 1 or #lines == 0 then
       return overridden(lines, phase)
     end
@@ -21,7 +26,14 @@ vim.paste = (function(overridden)
       return overridden(lines, phase)
     end
 
+    if config.get_option("debug") then
+      print("Line: " .. line)
+    end
+
     if not drag_and_drop.handle_paste(lines[1]) then
+      if config.get_option("debug") then
+        print("Did not handle paste, calling original vim.paste")
+      end
       return overridden(lines, phase) -- if drag and drop did not handle the paste, call the original vim.paste function
     end
   end
