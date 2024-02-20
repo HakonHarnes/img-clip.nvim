6b7461ff437b20b8d933a6963d545d21cae5d14c
Håkon Harnes <hakon@harnes.dev>
2024-02-20 10:29:07 +0100

    fix: add pasteImage function back

--- lua/img-clip/init.lua
+++ lua/img-clip/init.lua
@@ -1,5 +1,6 @@
 local clipboard = require("img-clip.clipboard")
 local config = require("img-clip.config")
+local paste = require("img-clip.paste")
 local util = require("img-clip.util")
 
 local M = {}
@@ -11,6 +12,12 @@ M.setup = function(opts)
   if not clipboard.get_clip_cmd() then
     util.error("Could not get clipboard command. See :checkhealth img-clip.")
   end
+end
+
+---@param opts? table
+---@param input? string
+M.pasteImage = function(opts, input)
+  return paste.paste_image(opts, input)
 end
 
 return M
